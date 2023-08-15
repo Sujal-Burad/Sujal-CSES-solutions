722032037f5927c8d996996391a85fe692b93ddc
SujalB99 <sujalburad99@gmail.com>
2023-08-15 16:47:12 +0530

    Create 28] Subarrays Sum 2.cpp

---
+++ Sorting and Searching/28] Subarrays Sum 2.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+#define ll  long long
+#define fio ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+using namespace std;
+#include<map> // for map operations
+#include<unordered_map>
+// #define mod 1000000007
+#include <algorithm>
+#include <vector>
+#include <iostream>
+#include <iterator>
+ 
+// Main Idea = This can be solved using the concept of prefix sum and maps. It is similar to the 'Subarrays Sum 1' problem.
+
+// Time Complexity = O(n).
+// Space Complexity = O(n), to store the prefix sums in map.
+
+int main()
+{
+    int n; cin>>n;
+    int tar; cin>>tar;
+    vector<int> vec(n);
+    for(int i=0; i<n; i++)
+        cin>>vec[i];
+ 
+    map<ll, ll> mpp;
+    mpp[0] = 1;
+    ll count = 0;
+    ll presum = 0;
+    for(int i=0; i<n; i++)
+    {
+        presum = presum + vec[i];
+        count = count + mpp[presum - tar];
+        mpp[presum]++;
+    }
+    cout<<count<<endl;
+    return 0;
+}
